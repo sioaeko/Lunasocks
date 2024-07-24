6378996273ae1de6fd1070d541cdcb989a07ab03
Sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-07-24 22:44:39 +0900

    Update server.go

--- server/server.go
+++ server/server.go
@@ -1,98 +1,47 @@
 package server
 
 import (
-    "errors"
-    "fmt"
-    "io"
     "log"
     "net"
     "time"
 
     "your_project/crypto"
-    "your_project/socks"
 )
 
-var (
-    ErrInvalidAddress = errors.New("invalid address")
-    ErrConnectionClosed = errors.New("connection closed unexpectedly")
-    ErrTimeout = errors.New("operation timed out")
-)
+type Server struct {
+    address  string
+    password string
+    timeout  time.Duration
+}
 
-func (s *Server) handleConnection(conn net.Conn) {
-    defer conn.Close()
-
-    cipher, err := crypto.NewCipher([]byte(s.password))
-    if err != nil {
-        log.Printf("Failed to create cipher: %v", err)
-        return
-    }
-
-    buf := make([]byte, 256)
-    n, err := conn.Read(buf)
-    if err != nil {
-        if errors.Is(err, io.EOF) {
-            log.Printf("Connection closed by client")
-        } else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-            log.Printf("Read timed out: %v", ErrTimeout)
-        } else {
-            log.Printf("Failed to read: %v", err)
-        }
-        return
-    }
-
-    decrypted, err := cipher.Decrypt(buf[:n])
-    if err != nil {
-        log.Printf("Failed to decrypt: %v", err)
-        return
-    }
-
-    addr, err := socks.ParseAddress(decrypted)
-    if err != nil {
-        log.Printf("Failed to parse address: %v", ErrInvalidAddress)
-        return
-    }
-
-    remote, err := net.DialTimeout("tcp", addr, 5*time.Second)
-    if err != nil {
-        if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-            log.Printf("Dial timed out: %v", ErrTimeout)
-        } else {
-            log.Printf("Failed to connect to %s: %v", addr, err)
-        }
-        return
-    }
-    defer remote.Close()
-
-    go func() {
-        if err := s.pipe(remote, conn, cipher.Decrypt); err != nil && !errors.Is(err, io.EOF) {
-            log.Printf("Error in remote to local pipe: %v", err)
-        }
-    }()
-
-    if err := s.pipe(conn, remote, cipher.Encrypt); err != nil && !errors.Is(err, io.EOF) {
-        log.Printf("Error in local to remote pipe: %v", err)
+func NewServer(addr, password string, timeout time.Duration) *Server {
+    return &Server{
+        address:  addr,
+        password: password,
+        timeout:  timeout,
     }
 }
 
-func (s *Server) pipe(dst io.Writer, src io.Reader, transform func([]byte) ([]byte, error)) error {
-    buf := make([]byte, 1024)
+func (s *Server) Start() error {
+    go s.handleUDP()
+
+    listener, err := net.Listen("tcp", s.address)
+    if err != nil {
+        return err
+    }
+    defer listener.Close()
+
+    log.Printf("Server listening on %s", s.address)
+
     for {
-        n, err := src.Read(buf)
-        if n > 0 {
-            data, err := transform(buf[:n])
-            if err != nil {
-                return fmt.Errorf("transform error: %w", err)
-            }
-            _, err = dst.Write(data)
-            if err != nil {
-                return fmt.Errorf("write error: %w", err)
-            }
-        }
+        conn, err := listener.Accept()
         if err != nil {
-            if err == io.EOF {
-                return nil
-            }
-            return fmt.Errorf("read error: %w", err)
+            log.Printf("Failed to accept connection: %v", err)
+            continue
         }
+
+        go s.handleConnection(conn)
     }
 }
+
+// handleConnection 함수는 이전과 동일하게 유지
